c0090fc5dc85dcfb265fd10010b61091dfc8cb85
Greg Elwood <greg_elwood@apple.com>
2017-06-29 15:05:30 -0700

    ~

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,13 @@
 'use strict';
 
+let dat = require('exdat');
 let Page = require('./Page/Page.js');
 
 let Main = (function() {
 	return {
 		initialize: function() {
 			this.setupScroll();
+			this.gui = new dat.GUI();
 		},
 		setupScroll: function() {
 			this.myPage = new Page(document.getElementById('main'));
